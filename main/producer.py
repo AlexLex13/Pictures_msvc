c1591eaf2e3c82f4321e2ec668914a6311e0be45
Alexey <suxovarov.alexey@gmail.com>
2023-01-23 13:09:16 +0300

    main producer

---
+++ main/producer.py
@@ -0,0 +1,12 @@
+import json
+import pika
+
+connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host="rabbitmq"))
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
